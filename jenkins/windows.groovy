693e1dfc1c6ea68b728c5eeba78cad342a35c740
Sven Jost <sven@wire.com>
2019-07-15 17:04:34 +0200

    Remove workspace variable

--- jenkins/windows.groovy
+++ jenkins/windows.groovy
@@ -16,7 +16,7 @@ node('node160') {
   stage('Probe signtool') {
     try {
       timeout(activity: true, time: 1) {
-        bat returnStatus: true, script: '"C:\\Program Files (x86)\\Windows Kits\\10\\bin\\x86\\signtool.exe" sign /t http://timestamp.digicert.com /a ${WORKSPACE}\\jenkins\\example.exe'
+        bat returnStatus: true, script: '"C:\\Program Files (x86)\\Windows Kits\\10\\bin\\x86\\signtool.exe" sign /t http://timestamp.digicert.com /a jenkins\\example.exe'
       }
     } catch(e) {
       currentBuild.result = 'FAILED'
